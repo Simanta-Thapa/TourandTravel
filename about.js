b05fef4569738581afbdb3f7f3edab505dd7a1a2
SIMANTA THAPA <megadsoul2005@gmail.com>
2024-01-09 21:55:17 +0545

    first commit

---
+++ about.js
@@ -0,0 +1,27 @@
+const navMenu = document.getElementById("nav-Menu");
+const navToggle = document.getElementById("nav-Toggle")
+const navClose = document.getElementById("nav-Close")
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>
+    {
+        navMenu.classList.add("showMenu")
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove("showMenu")
+      })
+}
+
+
+// header
+
+const blurHeader =() =>{
+    const header  = document.getElementById('header')
+    this.scrollY >=50 ? header.classList.add("blurHeader")
+                      : header.classList.remove("blurHeader")
+}
+
+window.addEventListener('scroll', blurHeader)

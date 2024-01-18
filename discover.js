73cd4556beb38b52c78c5628763de38fa710c094
SIMANTA THAPA <megadsoul2005@gmail.com>
2024-01-18 13:57:50 +0545

    Media queries of Discover Page

---
+++ discover.js
@@ -0,0 +1,40 @@
+// navbar 
+const navMenu = document.getElementById("nav-Menu");
+const navToggle = document.getElementById("nav-Toggle")
+const navClose = document.getElementById("nav-Close")
+
+
+if(navToggle){
+    navToggle.addEventListener('click',()=>
+    {
+        navMenu.classList.add("showMenu")
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove("showMenu")
+      })
+}
+
+
+// header
+
+const blurHeader =() =>{
+    const header  = document.getElementById('header')
+    this.scrollY >=50 ? header.classList.add("blurHeader")
+                      : header.classList.remove("blurHeader")
+}
+
+window.addEventListener('scroll', blurHeader)
+
+
+// show scroll up 
+
+const scrollUp = () =>{
+    const scrollUP = document.getElementById('scroll-up')
+    this.scrollY >=350 ? scrollUP.classList.add("showScroll")
+                       : scrollUP.classList.remove("showScroll")
+}
+
+window.addEventListener('scroll', scrollUp)
